1e74e89d1c780dce7488956580a1888172d39de2
Daniel <virusxd521@gmail.com>
2022-02-12 14:48:39 +0200

    Practice require and include

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <?php
+            require "./functions.php";
+
+            get_current_year();
+        ?>
+        © 2016 Me &amp; myself
+    </footer>
---
+++ page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!-- saved from url=(0065)https://classes.codingbootcamp.cz/assets/classes/12/html-doc.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>A website</title>
+</head>
+<body>
+    
+    <header>
+        A website
+        <nav>
+            <a href="./home.php">Home</a>
+            <a href="./page.php">Page #2</a>
+        </nav>
+    </header>
+
+    <div class="main">
+        <h1>The changed page</h1>
+
+        <p>Nulla adipisicing nulla nisi eu in qui reprehenderit exercitation cillum Lorem. Irure veniam aliqua est do velit dolor esse. Sint aute velit cillum ullamco cupidatat. Esse excepteur officia Lorem nostrud cillum est incididunt nostrud id ipsum ex. Non proident sint nisi consectetur excepteur incididunt fugiat laborum enim sit deserunt irure.</p>
+
+        <p>Est ullamco fugiat magna tempor laboris eu duis labore sit mollit proident. Deserunt velit elit culpa enim in laboris exercitation pariatur duis excepteur commodo cillum. Commodo ad et velit commodo reprehenderit est velit quis laborum anim aliquip in ut minim.</p>
+
+        <p>Sunt non laborum esse eiusmod anim irure. Adipisicing labore ex voluptate et magna reprehenderit. Fugiat minim magna non voluptate. Deserunt enim reprehenderit id mollit veniam ipsum.</p>
+    </div>
+
+    <footer>
+        © 2016 Me &amp; myself
+    </footer>
+
+
+</body></html>
